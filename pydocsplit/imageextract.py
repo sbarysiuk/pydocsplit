e1cc765f9ab4df1f619415150bb9a91bb345e986
Maxim Kamenkov <mkamenkov@gmail.com>
2013-05-04 19:47:15 +0300

    Added support for pages option for image extractor.

--- pydocsplit/imageextract.py
+++ pydocsplit/imageextract.py
@@ -26,48 +26,48 @@ class ImageExtractor:
             'density': DEFAULT_DENSITY,
             'rolling': True,
             }
-        self.info_extractor = InfoExtractor()  
-    
+        self.info_extractor = InfoExtractor()
+
     def extract(self, pdfs, **kwargs):
         """ Extracts images of each page in a PDF document
         Only supports to extract all pages in document at the moment
-        
+
         Usage:
-        
+
         >>>i = ImageExtractor()
-        >>>i.extract("/path/to/my/pdffile.pdf", output="/path/to/my/output/dir/", sizes=['500x', '250x'], formats=['png', 'jpg']) 
+        >>>i.extract("/path/to/my/pdffile.pdf", output="/path/to/my/output/dir/", sizes=['500x', '250x'], formats=['png', 'jpg'])
         """
-        
+
         self.options.update(kwargs)
-        
+
         for pdf in pdfs:
-            
+
             previous = None
             for s in self.options['sizes']:
                 for f in self.options['formats']:
                     self.convert(pdf, s.lower(), f.lower(), previous)
-                    
+
                 if self.options['rolling']:
                     previous = s.lower()
-                
+
     def normalize_option(self, key):
-            
+
             if type(self.options[key])==type(list()):
                 self.options[key] = ",".join([str(v) for v in self.options[key]])
 
     def resize_arg(self, size):
-        
+
         if size is None:
             return ''
         return "-resize %s" % size
-    
+
     def _copy_files(self,src, dest):
         src_files = os.listdir(src)
         for file_name in src_files:
             full_file_name = os.path.join(src, file_name)
             if (os.path.isfile(full_file_name)):
                 shutil.copy(full_file_name, dest)
-    
+
     def quality_arg(self, format):
         if format == "jpeg" or "jpg":
             return "-quality 85"
@@ -75,42 +75,60 @@ class ImageExtractor:
             return "-quality 100"
         else:
             format = ""
-        
+
+    @staticmethod
+    def _expand_page_sequence(value, delim='-'):
+        if isinstance(value, (int, long)):
+            return [value, ]
+        if delim in value:
+            pair = [int(x.strip()) for x in value.split(delim, 1) if x.isdigit()]
+            if len(pair) != 2:
+                return []
+            return range(pair[0], pair[1] + 1)
+        else:
+            return [int(value), ]
+
+    def pages_arg(self, pages):
+        result = []
+        [result.extend(self._expand_page_sequence(x)) for x in pages]
+        return list(sorted(set(result)))
+
     def convert(self, pdf, size, format, previous=None):
-        
+
         tempdir = tempfile.gettempdir()
         basename, ext = os.path.splitext(os.path.basename(pdf))
-        if size > 1: 
+        if size > 1:
             subfolder = str(size)
         else:
             subfolder = ''
-            
+
         directory = os.path.join(self.options['output'], subfolder)
-        
+
         if not os.path.exists(directory):
             os.makedirs(directory)
-        
-        #TODO add method to clean pages arg to range/list ref page_list in ruby ver
 
-        pages = range(1,self.info_extractor.extract('length', pdf)+1)
-        
+        if self.options.get('pages'):
+            pages = self.pages_arg(self.options['pages'])
+        else:
+            pages = range(1, self.info_extractor.extract('length', pdf) + 1)
+
         common = "%s -density %s %s %s" % (MEMORY_ARGS, self.options['density'], self.resize_arg(size), self.quality_arg(format))
-        
+
         if previous:
-            
+
             self._copy_files(os.path.join(self.options['output'], previous), directory)
             cmd = 'MAGICK_TMPDIR=%s OMP_NUM_THREADS=2 gm mogrify %s -unsharp 0x0.5+0.75 "%s/*.%s" 2>&1'% (tempdir, common, directory, format)
             self.run_gm(cmd.strip())
         else:
-            
+
             for page in pages:
                 out_file = os.path.join(directory, "%s_%i.%s" % (basename, page, format))
                 cmd = "MAGICK_TMPDIR=%s OMP_NUM_THREADS=2 gm convert +adjoin %s %s[%i] %s 2>&1" % (tempdir, common, pdf, (page-1), out_file)
                 self.run_gm(cmd.strip())
         return True
-    
+
     def run_gm(self, args):
-        
+
         procs = subprocess.Popen('%s' % args, shell=True, stdout=subprocess.PIPE)
 
         if procs.wait() != 0:
