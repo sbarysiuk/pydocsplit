7c903e9f502726f17beaab19b34d68193c9e65ee
Maxim Kamenkov <mkamenkov@gmail.com>
2013-08-08 10:48:53 +0300

    Added better extraction error support.

--- pydocsplit/exceptions.py
+++ pydocsplit/exceptions.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+
 class ExtractionError(Exception):
+
     def __init__(self, cmd, msg):
         self.cmd = cmd
         self.msg = msg
+        super(ExtractionError, self).__init__(cmd, msg)
--- pydocsplit/info_extractor.py
+++ pydocsplit/info_extractor.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import sys
-import os
 import re
+from pydocsplit.exceptions import ExtractionError
 from pydocsplit.command_runner import run, RunError
 
 
 class InfoExtractor:
-    
-    """ 
+    """
     Extracts meta data from pdf file. Returns value of meta field as string/integer
-    
+
     Valid meta data fields:
     author, date, creator, keywords, producer, subject, title, length
-    
+
     Usage:
     >>>i = InfoExtractor()
     >>>i.extract('title','/path/to/my/pdffile.pdf')
-    
+
     """
-    
+
     def __init__(self):
-        
+
         self.matchers = {
             'author': re.compile('^Author:\s+?([^\n]+)', re.MULTILINE | re.UNICODE),
             'date': re.compile('^CreationDate:\s+?([^\n]+)', re.MULTILINE | re.UNICODE),
@@ -31,26 +29,23 @@ class InfoExtractor:
             'producer': re.compile('^Producer:\s+?([^\n]+)', re.MULTILINE | re.UNICODE),
             'subject': re.compile('^Subject:\s+?([^\n]+)', re.MULTILINE | re.UNICODE),
             'title': re.compile('^Title:\s+?([^\n]+)', re.MULTILINE | re.UNICODE),
-            'length': re.compile('^Pages:\s+?([^\n]+)', re.MULTILINE| re.UNICODE),
+            'length': re.compile('^Pages:\s+?([^\n]+)', re.MULTILINE | re.UNICODE),
         }
-        
+
         self.keymatch = re.compile('^[\w\s]*?:\s+([^\n]+)')
-        
+
     def extract(self, key, pdfs, *args, **kwargs):
-        
+
         pdf = pdfs[0] if isinstance(pdfs, list) else pdfs
-        
+
         try:
             result = run('pdfinfo', '"%s"' % pdf)
-            lines = result.splitlines(True)
-        except RunError, err:
-            raise Exception, err
+            result.splitlines(True)
+        except RunError, e:
+            raise ExtractionError(e.cmd, e.msg)
         else:
             m = self.matchers[key].search(result)
             if m:
                 if key == "length":
                     return int(m.group(1).strip())
                 return m.group(1).strip()
-            else:
-                
-                return None
