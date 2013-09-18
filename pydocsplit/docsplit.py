6d3b9d73f6638e2b6f3b9b44a7d6b164e9a8e25b
Maxim Kamenkov <mkamenkov@gmail.com>
2013-09-19 01:32:12 +0300

    Fixed typo in office home definition. Improved error handling.

--- pydocsplit/docsplit.py
+++ pydocsplit/docsplit.py
@@ -29,21 +29,21 @@ else:
     office = None
 
 if office:
-    DOCSPLIT_OFFICEHOME = '-Doffice.home=%s" % office'
-else: 
+    DOCSPLIT_OFFICEHOME = '-Doffice.home=%s' % office
+else:
     DOCSPLIT_OFFICEHOME = ''
 
 DOCSPLIT_HEADLESS = '-Djava.awt.headless=true'
 
 class Docsplit:
-    
+
     def __init__(self):
         pass
-    
+
     def extract_pages(self, pdfs, **kwargs):
         """
         Extracts each page of a pdf file and saves as separate pdf files
-        
+
         Usage:
         >>>d = Docsplit()
         >>>d.extract_pages('[/path/to/my/document1.doc','/path/to/my/document2.doc'], output='/path/to/outputdir/', pages='1-2')
@@ -51,58 +51,58 @@ class Docsplit:
         pdf = self.ensure_pdfs(pdfs)
         p = PageExtractor()
         return p.extract(pdfs, **kwargs)
-    
+
     def extract_text(self, docs, **kwargs):
         """
         Extracts text from a PDF
-        The text is saved as a text file with same base name as your document in the 
-        output dir specified. 
-        
+        The text is saved as a text file with same base name as your document in the
+        output dir specified.
+
         Usage:
         >>>d = Docsplit()
         >>>d.extract_text('/path/to/my/pdffile.pdf', output='/path/to/outputdir/')
         >>>d.extract_text('/path/to/my/pdffile.pdf', output='/path/to/outputdir/', returntext=True)
         """
-        
+
         pdfs = self.ensure_pdfs(docs)
         t = TextExtractor()
         return t.extract(pdfs, **kwargs)
-    
-        
+
+
     def extract_pdf(self, docs, **kwargs):
         """
         Extracts pdf file from a document (i.e. .doc, .pdf, .rtf, .xls) using OpenOffice
-        
+
         Usage:
         >>>d = Docsplit()
         >>>d.extract_pdf('/path/to/my/document.doc', output='/path/to/outputdir/')
         """
         output = kwargs.get('output', '.')
-        
+
         if not os.path.exists(output):
-            os.makedirs(output)        
-        
+            os.makedirs(output)
+
         docs = [docs] if isinstance(docs, str) else docs
-        
+
         for doc in docs:
             filename, ext = os.path.splitext(os.path.basename(doc))
 
             options = '-jar %s/vendor/jodconverter/jodconverter-core-3.0-beta-4.jar -r %s/vendor/conf/document-formats.js' % (DOCSPLIT_JAVA_ROOT, DOCSPLIT_JAVA_ROOT)
-            
+
             args = ('%s %s %s/%s.pdf') % (options, doc, output, filename)
-            
-            return self._run(args, doc) 
-    
+
+            return self._run(args, doc)
+
     def ensure_pdfs(self, docs):
-            
-        """ 
+
+        """
         Makes sure the document exists as PDF, if not converts to PDF
         using office and saves in temp folder.
         """
-        
+
         #make sure we get a list of docs even though only a single doc is provided
         docs = [docs] if isinstance(docs, str) else docs
-        
+
         pdfs =[]
         for doc in docs:
             basename, ext = os.path.splitext(os.path.basename(doc))
@@ -114,16 +114,16 @@ class Docsplit:
                 self.extract_pdf([doc], output=tempdir)
                 pdfs.append("%s.pdf" % os.path.join(tempdir, basename))
         return pdfs
-    
+
     def extract_images(self, pdfs, **kwargs):
         """
         Extracts each page of a pdf file and saves as images of given size and format
-        
+
         Parameters:
         sizes: list of sizes i.e. ['500x', '250x']
         formats: list of formats i.e. ['jpg', 'png']
         pages (optional): list of pages either as list [1,2,5,6] or as a string in this format ['1-10']
-        
+
         Usage:
         >>>d = Docsplit()
         >>>d.extract_images('/path/to/my/pdffile.pdf', output='/path/to/outputdir/', sizes=['500x', '250x'], formats=['png', 'jpg'], pages=[1,2,5,7])
@@ -131,13 +131,13 @@ class Docsplit:
         pdfs = self.ensure_pdfs(pdfs)
         i = ImageExtractor()
         return i.extract(pdfs, **kwargs)
-    
+
     def extract_info(self, metakey, pdfs, **kwargs):
         """
         Extracts meta data from pdf file. Returns value of meta field as string
         Valid meta data fields:
         author, date, creator, keywords, producer, subject, title, length
-        
+
         Usage:
         >>>d = Docsplit()
         >>>d.extract_meta('/path/to/my/pdffile.pdf', 'title')
@@ -146,24 +146,22 @@ class Docsplit:
         pdfs = self.ensure_pdfs(pdfs)
         return i.extract(metakey,pdfs)
 
-        
+
     def _run(self, command, pdf, **kwargs):
-        
+
         """
         Private method to run the office document converter via java
         """
-        
+
         #TODO: Use args in subprocess and not shell=True
-        
+
         cmd = "java %s %s %s -cp %s %s" % (DOCSPLIT_HEADLESS, DOCSPLIT_LOGGING, DOCSPLIT_OFFICEHOME, DOCSPLIT_CLASSPATH, command)
-        
-        try: 
-            proc = subprocess.Popen(shlex.split(cmd),shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            
+
+        try:
+            proc = subprocess.Popen(shlex.split(cmd), shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         except Exception, e:
-            print e
-        
-        else: 
+            raise ExtractionError(cmd, "Failed to call underlying tool")
+        else:
             if proc.wait() != 0:
                 raise ExtractionError(cmd, proc.communicate()[0])
             else:
